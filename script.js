9f00650b0f45efa136b70a3dcc2b42a701c0d44c
Nouman <noumancodes@gmail.com>
2022-01-09 20:24:47 -0500

    Like button now spins when clicked

--- script.js
+++ script.js
@@ -57,12 +57,12 @@ function addCards(cards) {
                     <div class="buttons">
                         <button class="like-button">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" height="20px" width="20px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                             </svg>
                         </button>
                         <button class="info-button">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" height="20px" width="20px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                         </button>
                     </div>
@@ -81,7 +81,7 @@ function addCards(cards) {
                 if (like.classList.contains("liked")) {
                     like.classList.remove("liked");
                     like.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" height="20px" width="20px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                     </svg>`;
                 } else {
                     like.classList.add("liked");
@@ -89,6 +89,7 @@ function addCards(cards) {
                                         <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
                                     </svg>`;
                 }
+                setTimeout(() => like.blur(), 1000);
             });
 
             info.addEventListener("click", () => {
